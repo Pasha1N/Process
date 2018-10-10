3872b1a71512d7f5167d97962f926e47476d59e0
Паша <@DESKTOP-3C2NDOK>
2018-10-10 18:21:19 +0300

    Project Setup

---
+++ Process/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Process.ViewModels;
+using System.Windows;
+
+namespace Process
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ViewModelProcesses viewModelProcesses = new ViewModelProcesses();
+            Process.View.MainWindow mainWindow = new View.MainWindow(viewModelProcesses);
+            mainWindow.Show();
+        }
+    }
+}
---
+++ Process/View/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Process.ViewModels;
+using System.Windows;
+
+namespace Process.View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow(ViewModelProcesses viewModelProcesses)
+        {
+            InitializeComponent();
+
+            DataContext = viewModelProcesses;
+        }
+    }
+}
