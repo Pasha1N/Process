377e33bb22e2d695f102a5ca20918b0941e8d4a7
Паша <@DESKTOP-3C2NDOK>
2018-10-11 19:43:25 +0300

    structure creation !

--- Process/MethodsFromUnmanagedCode.cs
+++ Process/MethodsFromUnmanagedCode.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace Process
@@ -6,10 +7,31 @@ namespace Process
     internal class MethodsFromUnmanagedCode
     {
         private const string dllName = @"Kernel32.dll";
-        int idProcess = 0;
+        private int invalidValue = -1;
+        private ICollection<IntPtr> processDescriptors = new List<IntPtr>();
 
         [DllImport(dllName)]
 
         public static extern IntPtr CreateToolhelp32Snapshot(int TH32CS_SNAPPROCESS, int idProcess);
+
+
+
+        public void GetProcessHandle()
+        {
+            int invalidValue = -1;
+            IntPtr handle = CreateToolhelp32Snapshot(0x00000002, 0);
+            if (handle != (IntPtr)invalidValue)
+            {
+                
+
+
+            }
+
+
+
+        }
+
+
+
     }
 }
---
+++ Process/ProcessEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Process
+{
+    public struct ProcessEntry
+    {
+        public int DwSize { get; set; }
+        public int CntUsage { get; set; }//0
+        public int ProcessID { get; set; }//Идентификатор процесса.
+        public int DefaultHeapID { get; set; }//0
+        public int Th32ModuleID { get; set; }//0
+        public int Thread { get; set; }//поток
+        public int ParentProcessID { get; set; }//Идентификатор родительского процесса
+        public long PcPriClassBase { get; set; }//Базовый приоритет любых потоков, созданных этим процессом. (что это?)
+        public int Flags { get; set; }//0
+        public string ExeFile { get; set; }//путь к exe
+    }
+}
